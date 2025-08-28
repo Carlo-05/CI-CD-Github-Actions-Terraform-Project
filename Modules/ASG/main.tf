bb4997195e1dee08a8a39e4886ec0f7f3e730b28
Carlo Ralphael Bravo <104559034+Carlo-05@users.noreply.github.com>
2025-08-28 13:32:45 +0800

    Update main.tf

--- Modules/ASG/main.tf
+++ Modules/ASG/main.tf
@@ -24,7 +24,7 @@ resource "aws_launch_template" "WebApp_ASG_template" {
       name = var.iam_instance_profile
     }
     #user data, get sh file from github
-    user_data = base64encode(<<-EOF
+    user_data = <<-EOF
       #!/bin/bash
 
       # Log everything for debugging
@@ -69,7 +69,7 @@ resource "aws_launch_template" "WebApp_ASG_template" {
           exit 1
       fi
     EOF
-    )
+    
     tags = merge(var.default_tags, {Name = var.WebApp_ASG_template_tag })
 }
 
@@ -113,3 +113,4 @@ resource "aws_autoscaling_group" "WebApp_ASG" {
     }
     }
 }
+
