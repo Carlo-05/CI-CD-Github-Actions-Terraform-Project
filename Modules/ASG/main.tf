03d87aa5b206d7c74f746ea82aee472a60def715
Carlo Ralphael Bravo <104559034+Carlo-05@users.noreply.github.com>
2025-09-05 12:48:29 +0800

    Update main.tf

--- Modules/ASG/main.tf
+++ Modules/ASG/main.tf
@@ -42,27 +42,63 @@ resource "aws_launch_template" "WebApp_ASG_template" {
 
           #start ssm-agent
           sudo apt update -y
-          sudo apt install -y apache2
-          
           sudo snap start amazon-ssm-agent
           sudo snap services amazon-ssm-agent
 
-          # Start & Enable Apache
-          sudo systemctl start apache2
-          sudo systemctl enable apache2
+          # download script and execute
+          for i in {1..5}; do
+            if curl -fsSL -o /tmp/employees.sql https://raw.githubusercontent.com/Carlo-05/CI-CD-Github-Actions-Terraform-Project/refs/heads/main/Other%20Documents/employees.sql; then
+                echo "sql file download succeeded"
+                break
+            else
+                echo "Attempt downloading sql file failed, retrying in 10s..."
+                sleep 10
+            fi
+            done
+
+          for i in {1..5}; do
+            if curl -fsSL -o /tmp/ci-cd-project.sh https://raw.githubusercontent.com/Carlo-05/CI-CD-Github-Actions-Terraform-Project/refs/heads/main/Other%20Documents/ci-cd-project.sh; then
+                echo "script file download succeeded"
+                chmod 755 /tmp/ci-cd-project.sh
+                /tmp/ci-cd-project.sh
+                break
+            else
+                echo "Attempt downloading script file failed, retrying in 10s..."
+                sleep 10
+            fi
+            done
 
 
       elif echo "$OS_TYPE" | grep -q "Amazon Linux"; then
           
           #start ssm-agent
           sudo yum update -y
-          sudo yum install -y httpd
           sudo systemctl start amazon-ssm-agent
           sudo systemctl enable amazon-ssm-agent
 
-          # Start & Enable Apache
-          sudo systemctl start httpd
-          sudo systemctl enable httpd
+          # download script and execute
+          for i in {1..5}; do
+            if curl -fsSL -o /tmp/employees.sql https://raw.githubusercontent.com/Carlo-05/CI-CD-Github-Actions-Terraform-Project/refs/heads/main/Other%20Documents/employees.sql; then
+                echo "sql file download succeeded"
+                break
+            else
+                echo "Attempt downloading sql file failed, retrying in 10s..."
+                sleep 10
+            fi
+            done
+
+          for i in {1..5}; do
+            if curl -fsSL -o /tmp/ci-cd-project.sh https://raw.githubusercontent.com/Carlo-05/CI-CD-Github-Actions-Terraform-Project/refs/heads/main/Other%20Documents/ci-cd-project.sh; then
+                echo "script file download succeeded"
+                chmod 755 /tmp/ci-cd-project.sh
+                /tmp/ci-cd-project.sh
+                break
+            else
+                echo "Attempt downloading script file failed, retrying in 10s..."
+                sleep 10
+            fi
+            done
+
 
       else
           echo "OS not suppurted. This script supports Amazon Linux 2 and Ubuntu only."
@@ -113,4 +149,5 @@ resource "aws_autoscaling_group" "WebApp_ASG" {
     }
     }
 }
+
 
