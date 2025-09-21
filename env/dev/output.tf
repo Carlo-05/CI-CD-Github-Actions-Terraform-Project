bd3a31c654e11fb7784272751e48ab1ecef01c68
Carlo Ralphael Bravo <104559034+Carlo-05@users.noreply.github.com>
2025-09-21 11:25:45 +0800

    Update output.tf

--- env/dev/output.tf
+++ env/dev/output.tf
@@ -10,4 +10,8 @@ output "webapp-public-ip" {
 output "webapp-private-ip" {
   value = module.WEBAPP.webapp_instance_private_ip
 }
+#RDS
+output "rds_endpoint" {
+  value = split(":", module.RDS.db_endpoint)[0]
+}
 
