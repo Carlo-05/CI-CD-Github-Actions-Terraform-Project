6d3b714ad8137c2c56032ff1666f87ae135bc8d6
Carlo Ralphael Bravo <104559034+Carlo-05@users.noreply.github.com>
2025-09-22 13:44:21 +0800

    Update variables.tf

--- Other Documents/IAM-ROLE-GITHUBACTION/variables.tf
+++ Other Documents/IAM-ROLE-GITHUBACTION/variables.tf
@@ -1,15 +1,16 @@
 variable "region" {
   description = "value"
-  default = "us-west-2"
+  default = "<Desired Region>"
 }
 
 variable "github_repo" {
   description = "value"
-  default = "Carlo-05/CI-CD-Github-Actions-Terraform-Project"
+  default = "<Your Repository>/CI-CD-Github-Actions-Terraform-Project"
 }
 
 #tags
 variable "iam_role_githubaction_tag" {
   description = "value"
   default = "github_actions_test"
+
 }
